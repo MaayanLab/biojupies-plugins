addeca8c1d694df9fa98ee24c1c439371199199d
denis-torre <denis.torre@mssm.edu>
2018-07-05 11:42:39 -0400

    Added download results button to signature table

--- library/analysis_tools/signature_table/signature_table.py
+++ library/analysis_tools/signature_table/signature_table.py
@@ -33,6 +33,7 @@ def run(signature, signature_label=''):
 #############################################
 
 def plot(signature, plot_counter):
+	signature_txt = signature.to_csv(sep='\t')
 	# signature.index = ['<a href="http://www.genecards.org/cgi-bin/carddisp.pl?gene={x}" target="_blank">{x}</a>'.format(**locals()) for x in signature.index] # human
 	signature.index = ['<a href="http://amp.pharm.mssm.edu/Harmonizome/gene/'+x.upper()+'" target="_blank">'+x+'</a>' for x in signature.index] # mouse
 	signature.index = [index.replace('target="_blank">', 'target="_blank"><b>*').replace('</a>', '</b></a>') if rowData['adj.P.Val'] < 0.05 else index for index, rowData in signature.iterrows()] # mouse
@@ -44,6 +45,10 @@ def plot(signature, plot_counter):
 	display(HTML('<style>.w-100{width: 100%;}</style>'))
 	# display(qgrid.show_grid(signature.rename(columns={'gene_symbol': 'Gene', 'P.Value': 'P-value', 'adj.P.Val': 'FDR'}).drop(['t', 'B'], axis=1), grid_options={'maxVisibleRows': 4}))
 	display(HTML(html_results))
+
+	# Add download button
+	display(HTML('<textarea id="textbox" style="display: none;">{}</textarea> <button id="create">Download Results</button> <a download="signature.txt" id="downloadlink" style="display: none">Download</a>'.format(signature_txt)))
+	display(HTML('<script type="text/javascript">!function(){var e=null,t=document.getElementById("create"),n=document.getElementById("textbox");t.addEventListener("click",function(){var t,l,c=document.getElementById("downloadlink");c.href=(t=n.value,l=new Blob([t],{type:"text/plain"}),null!==e&&window.URL.revokeObjectURL(e),e=window.URL.createObjectURL(l)),c.click()},!1)}();</script>'))
 
 	# Figure Legend
 	display(Markdown('** Table '+plot_counter('table')+' | Differential Expression Table.** The figure displays a browsable table containing the gene expression signature generated from a differential gene expression analysis. Every row of the table represents a gene; the columns display the estimated measures of differential expression. Links to external resources containing additional information for each gene are also provided'.format(**locals())))
